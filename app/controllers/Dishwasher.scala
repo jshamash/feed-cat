e1d30874610e161fb6847edc4c96e798cfdcf4dd
Jake Shamash <jshamash@inocybe.com>
2015-03-02 21:14:15 -0500

    Added dishwasher feature

--- app/controllers/Dishwasher.scala
+++ app/controllers/Dishwasher.scala
@@ -1,12 +1,46 @@
 package controllers
 
-import controllers.Application._
-import play.api._
+import java.util.UUID
+
+import anorm._
+import play.api.db.DB
 import play.api.mvc._
+import play.api.Play.current
 
 object Dishwasher extends Controller {
 
-  def getDishwasher(id: Int) = Action {
-    Ok(id)
+  def createDishwasher = Action {
+    val uuid = UUID.randomUUID().toString
+    DB.withConnection { implicit c =>
+      SQL("insert into Dishwashers(dishwasher_id, isClean) values ({id}, {isClean});").on('id -> uuid, 'isClean -> false).executeInsert()
+    }
+    Ok(uuid)
+  }
+  
+  def getDishwasher(id: String) = Action {
+    // For validation
+    val uuid = UUID.fromString(id)
+    val isClean = DB.withConnection { implicit c =>
+      val selectDishwasher = SQL("Select isClean from Dishwashers where dishwasher_id={id}").on('id -> uuid.toString)
+      selectDishwasher().map(x => x[Boolean]("isClean")).head
+    }
+    Ok(views.html.dishwasher(uuid.toString, isClean))
+  }
+  
+  def toggleDishwasher(id: String) = Action {
+    // For validation
+    val uuid = UUID.fromString(id)
+    
+    val isClean = DB.withConnection { implicit c =>
+      val selectDishwasher = SQL("Select isClean from Dishwashers where dishwasher_id={id}").on('id -> uuid.toString)
+      val isClean = selectDishwasher().map(x => x[Boolean]("isClean")).head
+      
+      // update dishwasher
+      SQL("update Dishwashers set isClean={isClean} where dishwasher_id=({id});").on('id -> uuid, 'isClean -> !isClean).executeUpdate()
+      
+      !isClean
+    }
+
+    Ok(views.html.dishwasher(uuid.toString, isClean))
   }
 }
---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,7 @@
+# Add dishwashers
+
+# --- !Ups
+CREATE TABLE dishwashers(
+  dishwasher_id UUID PRIMARY KEY,
+  isClean BOOLEAN
+);
